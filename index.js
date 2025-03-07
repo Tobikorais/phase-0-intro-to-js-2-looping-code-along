10891344f33186e92bf7351c63a37bc84a334b40
Andrew Tobiko <andrew.tobiko1@student.moringaschool.com>
2025-03-07 22:15:14 +0300

    Adding files

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+
+// Function to return an array of thank you messages for each name provided
+function writeCards(names) {
+  let messages = [];
+  for (let i = 0; i < names.length; i++) {
+    messages.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+  }
+  return messages;
+}
+
+// Function to log each number counting down from the number provided to zero
+function countDown(number) {
+  while (number >= 0) {
+    console.log(number);
+    number--;
+  }
+}
